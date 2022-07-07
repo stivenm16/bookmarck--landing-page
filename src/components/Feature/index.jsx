adaa56aa1395d239a563d8c20087afb99808fe45
Kevin <stiivenmendez16@gmail.com>
2022-07-07 17:02:07 -0500

    States in features

--- src/components/Feature/index.jsx
+++ src/components/Feature/index.jsx
@@ -2,13 +2,15 @@ import './index.css'
 import React from 'react';
 import imgPrueba from '../../../images/illustration-1.svg'
 
-function Feature() {
+function Feature({title, text}) {
+    
+    console.log(text)
     return (
       <div className='feature-component'>
         <img src={imgPrueba} alt="" />
         <div className='feature-text-container'>
-            <h3 className='title-feature'>Bookmark in one click</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ad molestiae et, porro eligendi qui labore voluptas quam eum repellendus! Consectetur dolorum vitae optio esse sequi laboriosam laudantium amet illo?</p>
+            <h3 className='title-feature'>{title}</h3>
+            <p>{text}</p>
             <input type="button" value="more info"/>
         </div>
       </div>  
--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,24 +1,44 @@
 import './index.css'
-import React from 'react';
+import React, {useState} from 'react';
 import Feature from '../Feature';
 
 function Features() {
+
+    let [title, setTitle] = useState('Bookmark in one click')
+    let [textContent, setText] = useState('Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.')
+
     return (
         <div className="features">
             <h2 className='features-title'>Features</h2>
-            <p className='features-text'>Our aim is to make it quick and easy for you to access your favourite websites. 
-  Your bookmarks sync between your devices so you can access them on the go.</p>
+            <p className='features-text'>
+                Our aim is to make it  quick and easy for you to access your favourite websites. 
+                Your bookmarks sync between your devices so you can access them on the go.
+            </p>
             <nav className='features-nav'>
                 <ul className='features-options'>
-                    <li className='feature'>Simple Bookmarking</li>
+                    <li className='feature'>
+                        <input type="button" value="Simple Bookmarking" 
+                        onClick={() => setTitle('Bookmark in one click') & setText('Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.')}
+                        />
+                        
+                    </li>
                     
-                    <li className='feature'>Speedy Searching</li>
+                    <li className='feature'>
+                        <input type="button" value="Speedy Searching" 
+                        onClick={() => setTitle('Intelligent search') & setText('Our powerful search feature will help you find saved sites in no time at all.No need to trawl through all of your bookmarks.')}
+                        />
+                         
+                    </li>
                     
-                    <li className='feature'>Easy Sharing</li>
+                    <li className='feature'>
+                        <input type="button" value="Share your bookmarks" 
+                        onClick={() => setTitle('Share your bookmarks') & setText('Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.')}
+                        />
+                    </li>
                 </ul>
                 <hr className='line'/>
             </nav>
-            <Feature/>
+            <Feature title={title} text={textContent} />
         </div>
     );
   };
